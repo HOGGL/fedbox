120ab72cb286e3b2211fa868c54cb226cb8d6f37
Lachlan Hogg <41929615+HOGGL@users.noreply.github.com>
2021-12-04 14:07:53 +1000

    Finalise Pleroma package

--- boards/fedbox/post_build.sh
+++ boards/fedbox/post_build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cat >> $1/etc/fstab <<EOF
-/dev/sda1	/var/lib	xfs	defaults	0	0
+#/dev/sda1	/var/lib	xfs	defaults	0	0
 EOF
+
+mkdir -p $1/var/lib/pleroma/{uploads,static} $/etc/pleroma
--- package/pleroma/pleroma.mk
+++ package/pleroma/pleroma.mk
@@ -1,42 +1,55 @@
-# ifdef BR2_arm
-# 	PLEROMA_FLAVOUR = "arm"
-# endif
-
-# ifdef BR2_aarch64
-# 	PLEROMA_FLAVOUR = "arm64"
-# endif
-
-# ifdef BR2_x86_64
-# 	PLEROMA_FLAVOUR = "amd64"
-# endif
-
-# ifeq ($(BR2_TOOLCHAIN_BUILDROOT_LIBC),"musl")
-# 	PLEROMA_FLAVOUR := $(PLEROMA_FLAVOUR)"-musl")
-# endif
-
 PLEROMA_VERSION = v2.4.1
 PLEROMA_SITE = https://git.pleroma.social/pleroma/pleroma.git
 PLEROMA_SITE_METHOD = git
 PLEROMA_LICENSE = AGPL-3
 PLEROMA_LICENSE_FILES = COPYING AGPL-3
 PLEROMA_DEPENDENCIES = host-elixir host-cmake host-file erlang file
+# Add exiftool to strip location data from uploaded images
+
+PLEROMA_TARGET_DEST = $(TARGET_DIR)/opt/pleroma
 
 define PLEROMA_BUILD_CMDS
-	sed -i '/include_executables_for/a include_erts: false,' \
-		$(@D)/mix.exs
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) -f $(PLEROMA_PKGDIR)/Makefile
 endef
 
 define PLEROMA_INSTALL_TARGET_CMDS
-	cp -r $(@D)/release $(TARGET_DIR)/opt/pleroma
-# $(INSTALL) -d -m 755 $(TARGET_DIR)/opt/pleroma \
-# 	$(TARGET_DIR)/opt/pleroma/{bin,lib,releases}
-# $(foreach dir, $(wildcard $(@D)/release/lib/*),
-# 	$(eval lib = $(dir:$(@D)/release/lib/%=%))
-# 	$(INSTALL) -d -m 755 $(TARGET_DIR)/opt/pleroma/lib/$(lib)
-# 	$(INSTALL) -d -m 755 $(TARGET_DIR)/opt/pleroma/lib/$(lib)/ebin
-# 	$(INSTALL) -m 644 $(wildcard $(dir)/ebin/*) \
-# 		$(TARGET_DIR)/opt/pleroma/lib/$(lib)/ebin)
+	cd $(@D)/release; find lib releases -type f \! -executable -exec \
+		$(INSTALL) -D -m 644 "{}" "$(PLEROMA_TARGET_DEST)/{}" \;
+
+	$(INSTALL) -d -m 755 $(PLEROMA_TARGET_DEST)/bin
+	$(INSTALL) -m 755 $(@D)/release/bin/* $(PLEROMA_TARGET_DEST)/bin
+
+	cd $(@D)/release; find releases -type f -executable \
+		\( -name "elixir" -o -name "iex" \) -exec \
+		$(INSTALL) -D -m 755 "{}" "$(PLEROMA_TARGET_DEST)/{}" \;
+
+	$(INSTALL) -d -m 755 \
+		$(TARGET_DIR)/var/lib/pleroma/{uploads,static} \
+		$(TARGET_DIR)/etc/pleroma
+endef
+
+define PLEROMA_USERS
+	pleroma 60 social 60 - /opt/pleroma - - Twitter alternative
+endef
+
+define PLEROMA_PERMISSIONS
+	/var/lib/pleroma r 750 pleroma social - - - - -
+	/etc/pleroma d 755 pleroma social - - - - -
+endef
+
+define PLEROMA_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 -D $(PLEROMA_PKGDIR)/S90pleroma \
+		$(TARGET_DIR)/etc/init.d
+endef
+
+define PLEROMA_INSTALL_INIT_OPENRC
+	$(INSTALL) -D -m 0755 $(@D)/release/installation/init.d/pleroma \
+		$(TARGET_DIR)/etc/init.d
+endef
+
+define PLEROMA_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 $(@D)/release/installation/pleroma.service \
+		$(TARGET_DIR)/usr/lib/systemd/system
 endef
 
 $(eval $(generic-package))
